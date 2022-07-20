b173ac23a30dfb4b1275f04775ff4413796e9194
refaldy04 <refaldybanggana@gmail.com>
2022-07-21 06:01:06 +0700

    implementation axios

--- src/App.js
+++ src/App.js
@@ -1,31 +1,61 @@
 import React from 'react';
+import { Container, Col, Row, Figure } from 'react-bootstrap';
+const axios = require('axios').default;
 
-class Series extends React.Component {
-  render() {
-    return <li>{this.props.name}</li>;
-  }
-}
+// axios
+//   .get('https://rickandmortyapi.com/api/character')
+//   .then(function (response) {
+//     // handle success
+//     console.log(response.data.results[0].image);
+//   })
+//   .catch(function (error) {
+//     // handle error
+//     console.log(error);
+//   });
+// .then(function () {
+//   // always executed
+// });
 
-class Counter extends React.Component {
+// class Series extends React.Component {
+//   render() {
+//     return <li>{this.props.name}</li>;
+//   }
+// }
+
+class Image extends React.Component {
   state = {
-    count: 0,
+    image: [],
+    name: '',
   };
-  increase = () => {
-    this.setState(({ count }) => ({
-      count: count + 1,
-    }));
-  };
-  decrease = () => {
-    this.setState(({ count }) => ({
-      count: count - 1,
-    }));
-  };
+
+  componentDidMount() {
+    axios.get(`https://rickandmortyapi.com/api/character`).then((res) => {
+      // console.log(res.results);
+      res.data.results.map((e) => {
+        return this.state.image.push(e.image);
+      });
+      // const image = res.data.results[0].image;
+      const name = res.data.results[0].name;
+      // this.setState({ image });
+      this.setState({ name });
+
+      // console.log(this.state.image[1]);
+    });
+  }
+
   render() {
     return (
       <>
-        <button onClick={this.increase}>Increase</button>
-        <h1>{this.state.count}</h1>
-        <button onClick={this.decrease}>Decrease</button>
+        <Container fluid>
+          <Row>
+            <Col className="vh-100 d-flex justify-content-center align-items-center">
+              <Figure className="text-center">
+                <Figure.Image width={171} height={180} alt="171x180" src={this.state.image[1]} />
+                <Figure.Caption>{this.state.name}</Figure.Caption>
+              </Figure>
+            </Col>
+          </Row>
+        </Container>
       </>
     );
   }
@@ -36,7 +66,7 @@ class App extends React.Component {
     return (
       <>
         <div>
-          <Counter />
+          <Image />
         </div>
       </>
     );
